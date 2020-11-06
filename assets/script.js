e23c2582c2c33f9f5c1770da9df639ffd01b8f79
JordonMoses <jordon@blackbirdrevolt.com>
2020-11-06 13:06:16 -0600

    UV Index API added and classes to change UV color added

--- assets/script.js
+++ assets/script.js
@@ -1,54 +1,132 @@
-// Query Selectors 
-let APIKey = "e4cea2cf502cc4322a8fb98afebebc62";
+// Query Selectors & Variables
+let APIKey = "50012762a1eb2a9ece8edda2d39d7fc2";
 let searchButton = document.querySelector("#search-button");
 let citySearchValue = document.querySelector("#city-search");
 
-let searchHistory = document.querySelector("#history");
+let searchHistory = $("#history");
 let newCity = $("<li>");
+newCity.addClass("list-group-item btn")
 
-
-
-// Variables
+// Array
+let cities = []
 
 
 // Functions 
-function findWeather () {
+function findWeather() {
     let cityName = citySearchValue.value;
-    localStorage.setItem("city", JSON.stringify(cityName));
-    displayWeather ()
+    cities.push(cityName)
+    localStorage.setItem("city", JSON.stringify(cities));
+    displayWeather()
 }
 
-function displayWeather () {
-    JSON.parse(localStorage.getItem("city"));
+function displayWeather() {
+    let userCity = JSON.parse(localStorage.getItem("city"));
+    newCity.text(userCity)
     searchHistory.prepend(newCity);
 }
 
+function renderCityButtons() {
+
+    $("#history").empty();
+
+    // Loops through the array of movies
+    for (var i = 0; i < cities.length; i++) {
+
+        let li = $("<li>");
+        // Adds a class of movie to our button
+        li.addClass("list-group-item btn")
+
+        // Button text
+        li.text(cities[i]);
+        // Added the button to #history list
+        $("#history").prepend(li);
+    }
+}
 
 
+// On click saves input to local storage
+$("#search-button").on("click", function (event) {
+    event.preventDefault();
+
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearchValue.value + "&appid=" + APIKey;
+    console.log(queryURL)
 
 
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+        console.log(response)
+
+        let cityTemp = (response.main.temp-273.15)*1.8+32;
+        let cityHumidity = response.main.humidity
+        let cityWind = response.wind.speed
+
+        let latitude = response.coord.lat
+        let longitude = response.coord.lon
+        let queryURLTwo = "http://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=" + APIKey;
+
+         console.log(longitude)
+
+        $.ajax({
+            url: queryURLTwo,
+            method: "GET"
+        }).then(function (response) {
+
+            console.log(response.value)
+
+            let cityUV = response.value
+            let u = $("<p>").text("UV Index: " + cityUV);
+            $("#selectedCity").append(u)
+
+            if (cityUV >= 0 && cityUV < 3){
+                $(u).addClass("favorable")
+            } else if (cityUV >=3 && cityUV < 8){
+                $(u).addClass("moderate")
+            } else if (cityUV >= 8){
+                $(u).addClass("severe")
+            }
+
+        })
 
 
+        let t = $("<p>").text("Temperature: " + cityTemp.toFixed(2));
+        let h = $("<p>").text("Humidity: " + cityHumidity);
+        let w = $("<p>").text("Wind Speed: " + cityWind);
+        
 
+        $("#cityDate").text(response.name + moment().format(" - MMM Do"));
+        $("#selectedCity").html(t)
+        $("#selectedCity").append(h)
+        $("#selectedCity").append(w)
+        
 
-// Prepend to search history
-// newCity.textContent = citySearchValue.value
-// searchHistory.prepend(newCity)
+    });
 
-
+    findWeather()
+    renderCityButtons();
+});
 
 
 // AJAX 
-// let queryURL = "api.openweathermap.org/data/2.5/weather?q=" + "citySearchValue" + "&appid=" + APIKey;
 
 
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   }).then(updatePage); // need to write update page function 
+
+
+
+
+
+// ------------------------------ Other code
+
+
+
+ // let userCityInput = citySearchValue.value
+
+// cities.push(userCityInput);
 
 
 // Buttons
-$(document).on("click", "#search-button", findWeather);
+// $(document).on("click", "#search-button", findWeather);
 
 
